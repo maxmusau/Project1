bc024e1b0025617323a334eaed202790a255b604
maxmusau <musaumaxwell@gmail.com>
2022-11-30 17:58:18 +0300

    project committed files

--- app/src/main/java/com/example/project1/Signup.kt
+++ app/src/main/java/com/example/project1/Signup.kt
@@ -82,13 +82,21 @@ class Signup : AppCompatActivity() {
                         //super.onFailure(statusCode, headers, throwable, errorResponse)
                     }
 
-
+// https://github.com/maxmusau/Project1.git
                 }
             )
 
         }
-
-
+//echo "# Project1" >> README.md
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/maxmusau/Project1.git
+//git push -u origin main
+//git remote add origin https://github.com/maxmusau/Project1.git
+//git branch -M main
+//git push -u origin main
 
     }
 }
